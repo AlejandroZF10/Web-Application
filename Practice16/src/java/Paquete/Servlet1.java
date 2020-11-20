3cfff78238e41e2f5427aa93a708cc3ceaf0b88b
Alejandro Zepeda <alexzepeda07@hotmail.com>
2020-11-20 10:02:01 -0600

    Práctica 16 - Matriz dinámica

---
+++ Practice16/src/java/Paquete/Servlet1.java
@@ -0,0 +1,38 @@
+package Paquete;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            response.setContentType("text/html;charset=UTF-8");
+            String filas1 = request.getParameter("filas");
+            String columnas1 = request.getParameter("columnas");
+            int contador = 0;
+            int filasint = Integer.parseInt(filas1);
+            int columnasint = Integer.parseInt(columnas1);
+            PrintWriter out = response.getWriter();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<table border = '2'>");
+            for(int i=0;i<filasint;i++){
+                out.println("<tr>");
+                for(int j=0;j<columnasint;j++){
+                    out.println("<td>Celda"+i+j+"</td>");   
+                }
+                out.println("</tr>");
+            }
+            out.println("</body>");
+            out.println("</html>"); 
+    }
+}
